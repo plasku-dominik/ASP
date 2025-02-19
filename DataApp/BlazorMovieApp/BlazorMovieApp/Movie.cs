84ee72842b2d26f60697490c508026fa5693f9c4
DMNK <plasku.dominik1217@gmail.com>
2025-02-19 15:23:02 +0100

    forms & validation testing

--- DataApp/BlazorMovieApp/BlazorMovieApp/Movie.cs
+++ DataApp/BlazorMovieApp/BlazorMovieApp/Movie.cs
@@ -5,10 +5,14 @@ namespace BlazorMovieApp
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string? Title { get; set; }
         [DataType(DataType.Date)]
         public DateTime? ReleaseDate { get; set; }
         public string? Genre { get; set; }
+
+        [Range(0, 100)]
         public decimal Price { get; set; }
     }
 }
