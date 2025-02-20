a628934930eb9332e1a2db85eaece4b457760f31
DMNK <plasku.dominik1217@gmail.com>
2025-02-20 22:54:24 +0100

    creating a Web API
    - created a simple create webapi for a todo list

---
+++ WebAPIApp/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+var todos = new List<Todo>();
+
+app.MapGet("/todos", () => todos);
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+{
+    var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+    return targetTodo is null
+        ? TypedResults.NotFound()
+        : TypedResults.Ok(targetTodo);
+});
+
+app.MapPost("/todos", (Todo task) => 
+{
+    todos.Add(task);
+    return TypedResults.Created("/todos/{id}");
+});
+
+app.MapDelete("/todos/{id}", (int id) => 
+{
+    todos.RemoveAll(t => id == t.Id);
+    return TypedResults.NoContent();
+});
+
+app.Run();
+
+public record Todo(int Id, string Name, DateTime DueDate, bool IsCompleted) {}
